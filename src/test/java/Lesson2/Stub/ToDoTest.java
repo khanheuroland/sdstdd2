9511874a3af568dfe9eae977adbe2780901fa2d0
Khanh Tran <khanh.euroland@gmail.com>
2025-09-17 10:02:12 +0700

    reduce coverage

--- src/test/java/Lesson2/Stub/ToDoTest.java
+++ src/test/java/Lesson2/Stub/ToDoTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ToDoTest {
+    @Test
     public void should_return_a_list_of_tasks_after_remove_a_task()
     {
         //Create stub
@@ -21,6 +22,7 @@ public class ToDoTest {
         assertThat(tasks, allOf(not(hasItem(taskToRemove)), hasItem("Go to cinema")));
     }
 
+    @Test
     public void should_throw_exception_if_remove_unavailable_task()
     {
         ITodoRepository repoStub = new TodoRepositoryStub();
